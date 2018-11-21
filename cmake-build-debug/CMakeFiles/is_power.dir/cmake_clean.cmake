3201807eb242974344fed7f7fb5f4bc30b2288ba
victorsmits <victor.smits@student.vinci.be>
2018-11-21 11:54:46 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/is_power.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/is_power.dir/main.c.o"
+  "is_power.pdb"
+  "is_power"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/is_power.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
